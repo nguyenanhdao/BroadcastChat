12439720b228582d7cadbaa464762d7de888ba6a
Nguyen Dao Anh <nguyen.anh.dao@steadfasttech.com.au>
2016-04-07 08:41:00 +0700

    02 - Create basic Authorization level

---
+++ api-v1/user-authorization-type.js
@@ -0,0 +1,12 @@
+﻿(function (module) {
+
+    function UserAuthorizationType() {
+    
+    };
+    
+    UserAuthorizationType.UnAuthorizationUser = 0;
+    UserAuthorizationType.AuthorizationUser = 1;
+    UserAuthorizationType.AuthorizationAndUnAuthorizationUser = 2;
+
+    module.exports = UserAuthorizationType;
+})(module);

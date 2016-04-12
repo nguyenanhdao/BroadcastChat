52315b9f409bbc1832a3a29d1e958ea3e2e2d2b0
Nguyen Dao Anh <nguyen.anh.dao@steadfasttech.com.au>
2016-04-12 08:37:44 +0700

    03 - Create UserLocation schema

---
+++ data-access/models/user-location.js
@@ -0,0 +1,36 @@
+﻿//
+// Required library
+//
+var Mongoose = require('mongoose');
+var Schema = Mongoose.Schema;
+var Validation = require('../validation');
+
+(function (UserLocation) {
+    UserLocation.MODEL_NAME = 'UserLocation';
+
+    //
+    // Define user Schema
+    //
+    var userLocationSchema = new Schema({
+        longitude: {
+            type: Number,
+            require: true
+        },
+
+        latitude: {
+            type: Number,
+            require: true
+        },
+
+        createdWhen: {
+            type: Date,
+            default: Date.now
+        }
+    });
+
+    //
+    // Register schema with Mongoose
+    //
+    UserLocation = Mongoose.model(UserLocation.MODEL_NAME, userLocationSchema);
+
+})(module.exports);

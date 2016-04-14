328ade714a9a423b41814014518294895a8f0090
nguyenanhdao <nguyen.anh.dao@steadfasttech.com.au>
2016-04-14 21:30:59 +0700

    03 - Add PublicMessageMO

--- data-access/database-context.js
+++ data-access/database-context.js
@@ -15,12 +15,14 @@ var Promise = require('promise');
     //
     var User = require('./models/user.js');
     var UserLocation = require('./models/user-location.js');
+    var PublicMessage = require('./models/public-message.js');
 
     //
     // Register schema with database context
     //
     DatabaseContext.User = Mongoose.model(User.MODEL_NAME);
     DatabaseContext.UserLocation = Mongoose.model(UserLocation.MODEL_NAME);
+    DatabaseContext.PublicMessage = Mongoose.model(PublicMessage.MODEL_NAME);
 
     module.exports = DatabaseContext;
 })(module);
---
+++ data-access/models/public-message.js
@@ -0,0 +1,45 @@
+﻿//
+// Required library
+//
+var Mongoose = require('mongoose');
+var Schema = Mongoose.Schema;
+
+(function (PublicMessage) {
+    PublicMessage.MODEL_NAME = 'PublicMessage';
+
+    //
+    // Define public message
+    //
+    PublicMessage.SCHEMA = new Schema({
+        message: {
+            type: String,
+            require: true
+        },
+
+        longitude: {
+            type: Number,
+            require: true
+        },
+
+        latitude: {
+            type: Number,
+            require: true
+        },
+
+        createdWhen: {
+            type: Date,
+            default: Date.now
+        },
+
+        createdWho: {
+            type: String,
+            require: true
+        }
+    });
+
+    //
+    // Register schema with Mongoose
+    //
+    PublicMessage = Mongoose.model(PublicMessage.MODEL_NAME, PublicMessage.SCHEMA);
+
+})(module.exports);
